bce107dfb3aab6bb72fc18fc72a70633fab1249a
Freemakersoul <freemakersoul@gmail.com>
2024-12-02 17:10:14 +0000

    02/12/2024

---
+++ TiagoProença/02/12/2024/CPP_TP_02 _V2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int idade;
+    float altura;
+
+    cout << "Introduza a sua idade e a sua altura" << endl;
+    cin >> idade >> altura;
+    cout << endl << "A sua idade e' " << idade << " anos e a sua altura e' " << altura << "m"; 
+
+    return 0;
+}
