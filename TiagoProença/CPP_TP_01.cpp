8f1ccae1ed5df977dd2b1616dc78886a81182cb3
Freemakersoul <freemakersoul@gmail.com>
2024-12-02 15:12:30 +0000

    CPP_TP_01

---
+++ TiagoProença/CPP_TP_01.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int numero;
+
+    cout << "Digite um numero inteiro: ";
+    cin >> numero;
+    cout << "O numero introduzidofoi: " << numero << endl;
+
+    return 0;
+}
